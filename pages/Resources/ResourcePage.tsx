ef26bbd769afe5e97f6988a2787fb55c58d4d1b5
eSantos16 <earl.santos@ernox.com>
2025-10-28 21:20:04 +1100

    20251028_03 - Added Expo File System; Updated ResourcePage logic: on load -> check if file exists locally -> if yes then load local copy | if no then call api, save locally and load;

--- pages/Resources/ResourcePage.tsx
+++ pages/Resources/ResourcePage.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { View, Dimensions } from "react-native";
+import { View, Dimensions, Platform } from "react-native";
 import { useTheme, IconButton} from "react-native-paper";
 import {WebView} from "react-native-webview"
 import * as LucideIcons from "lucide-react-native";
 import CustomText from "../../assets/CustomText";
 
+import { File, Paths } from 'expo-file-system';
 
 import {StackScreenProps} from "@react-navigation/stack";
 import { ResourceStackParamList} from "../../types/AppTypes";
@@ -22,11 +23,23 @@ const loadResource = async (Path: string, FileType: string) => {
   return response.data;
 };
 
+const storeResource = async (contentString: string, filename:string, fileType: string) => {
+  try {
+    const src = new File(Paths.document, filename);
+    const bytes = fileType == "application/pdf" ? Buffer.from(contentString, "base64") : new TextEncoder().encode(contentString)
+    src.write(bytes)
+    
+    // console.log('File saved successfully:', src);
+  } catch (error) {
+    console.error('Error saving file from Base64:', error);
+  }
+}
+
 type props = StackScreenProps<ResourceStackParamList, "Resource">;
 
 const ResourcePage = ({ route, navigation }: props) => {
-  const [resource, setResource] = useState();
-  const [pdfBase64, setPdfBase64] = useState("")
+  const [resource, setResource] = useState<any>();
+  const [pdfSource, setPdfSource] = useState("")
   const [htmlContent, setHtmlContent] = useState<any>()
 
   const theme = useTheme();
@@ -36,14 +49,26 @@ const ResourcePage = ({ route, navigation }: props) => {
   const fileType = params.FileType;
 
   useEffect(() => {
-    loadResource(accessRid, fileType).then((res) => setResource(res));
+    const file = new File(Paths.document, displayName);
+
+    if(file.exists){
+      if(fileType == "application/pdf"){
+        setPdfSource(file.uri)
+      }else {
+        setHtmlContent(file.textSync())
+      }
+    }else {
+      loadResource(accessRid, fileType).then((res) => setResource(res));
+    }
   }, []);
 
   useEffect(() => {
     if(resource && fileType == "application/pdf"){
       const base64String = Buffer.from(resource as any, "binary").toString("base64");
-      setPdfBase64(base64String)
+      storeResource(base64String, displayName, fileType)
+      setPdfSource(`data:application/pdf;base64,${base64String}`)
     }else if(resource && fileType == "text/html"){
+      storeResource(resource, displayName, fileType)
       setHtmlContent(resource)
     }
   }, [resource])
@@ -60,7 +85,7 @@ const ResourcePage = ({ route, navigation }: props) => {
           javaScriptEnabled={true}
           style={{flex: 1,}}
       />}
-      {pdfBase64 && <Pdf source={{uri:`data:application/pdf;base64,${pdfBase64}`}} style={{flex: 1, width: Dimensions.get("window").width, height: Dimensions.get("window").height}} trustAllCerts={false} />}
+      {pdfSource && <Pdf source={{uri:pdfSource}} style={{flex: 1, width: Dimensions.get("window").width, height: Dimensions.get("window").height}} trustAllCerts={false} />}
     </View>
   );
 };
